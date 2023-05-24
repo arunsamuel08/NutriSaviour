ce10a06608d648111f53425a325dfaeda461992a
Arun Saji Samuel <arun.samuel08@gmail.com>
2023-05-25 00:49:17 +0530

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="container">
+          <div className="left-div">
+            <div className="header">
+              <div className="main-head">
+                <img src="./images/health.png" alt="health" />
+              </div>
+              <div className="sub-head">
+                <h1>NutriSaviour</h1>
+                <p>Elevate your plate with smart nutrition insights.</p>
+              </div>
+            </div>
+
+            <div className="search-cont">
+              <input className="input-box" placeholder="100 gm of Paneer" />
+              <button className="search-btn">
+                <img src="./images/search.png" alt="search" />
+              </button>
+            </div>
+          </div>
+          <div className="right-div"></div>
+        </div>
+      </div>
+    </>
   );
 }
 
